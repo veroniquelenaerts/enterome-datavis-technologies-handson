74b0db75e5ff01e99d164fac071a8a5d486195c3
veroniquelenaerts <veronique.lenaerts@live.com>
2023-04-23 15:46:00 +0200

    Finish web technologies exercises

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -2,16 +2,23 @@
   let current = "";
 </script>
 
+<style>
+  .red_btn {
+    background-color: red;
+  }
+</style>
+
 <!-- We will cover buttons later on! -->
-<button class="btn btn-secondary" on:click={() => (current = "messi")}>
+<button class="btn btn-secondary" class:red_btn={current == "messi"} on:click={() => (current = "messi")}>
   Messi 
 </button>
 
-<button class="btn btn-secondary" style='background-color:red' on:click={() => (current = "ronaldo")}>
+<button class="btn btn-secondary" class:red_btn={current == "ronaldo"}  on:click={() => (current = "ronaldo")}>
   Ronaldo
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "neymar")}>
+<button class="btn btn-secondary" class:red_btn={current == "neymar"} on:click={() => (current = "neymar")}>
   Neymar
 </button>
+
 
